6b23c7e84b2a0aaefe2905f3ed2f2a0eba769a47
lolocro <bosniclovro@gmail.com>
2025-03-25 22:45:46 +0100

    Added auth guard to orders

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [OrderController],
   providers: [PrismaService, OrderService]
 })
